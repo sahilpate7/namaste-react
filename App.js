249e8d01b9e2e48561a89fe7d27d59d16557d199
Sahil Pate <sahilpate7@gmail.com>
2025-01-19 22:23:14 +0530

    start and build script added

--- App.js
+++ App.js
@@ -1,21 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
-// rested elements
-const parent = React.createElement("div", {id: "parent"},[
-    React.createElement("div", {id: "child"},[
-        React.createElement("h1", {}, "I am h1"),
-        React.createElement("h2", {}, "I am h2")
-    ]),
-    React.createElement("div", {id: "child2"},[
-        React.createElement("h1", {}, "I am h1"),
-        React.createElement("h2", {}, "I am sahil")
-    ])
-]);
-
-console.log(parent);
+const jsxHeading =<h1 id="heading">Heading from JSX</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(parent);
+root.render(jsxHeading);
